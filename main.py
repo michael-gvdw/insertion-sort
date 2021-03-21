83a14763cdfda950d278064b201f5be10a04740b
Michael Groenewegen van der Weijden <michael@fragrant-chios.com>
2021-03-21 17:00:54 +0100

    insertion sort implementation

---
+++ main.py
@@ -0,0 +1,16 @@
+# insertion sort project
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+
+if __name__ == '__main__':
+    val = insertion_sort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92])
+    print(val)
